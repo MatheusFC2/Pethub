86ee197ba9cc9f92e16a4a5d8a229741b2d68425
GlatzCampos <gabrielglatzc@gmail.com>
2023-09-18 21:35:33 -0300

    New design and Navbar almost ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+function NavBar(){
+    return(
+        <>
+            <header className="flex w-screen items-center justify-between px-20 py-7 shadow-md">
+                <div className="flex align-middle justify-center ">
+                    <div className="w-48 h-10">
+                        <img src="./src/images/logo.svg" alt="" />
+                    </div>
+                    <div id="menu" className="pl-4 align-middle">
+                        <ul className="flex text-lg font-semibold space-x-4 text-gray-500 ">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Encontrar Herói</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Como funciona</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Ajuda</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Quero ser herói</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="conta" className="">
+                    <a href="">
+                        <span className="text-xl font-semibold transition ease-in-out delay-150 text-gray-400 hover:text-gray-600 duration-300">Criar conta ou entrar</span>
+                    </a>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default NavBar;
